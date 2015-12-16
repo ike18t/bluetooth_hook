635d27a8d96945a383a248273409b8e6dc9737a8
Rex <rexrennie@gmail.com>
2015-12-16 11:06:53 -0500

    Ike/Rex | improving tests for bluetooth low energy scanner

--- lib/bluetooth_low_energy_scanner.rb
+++ lib/bluetooth_low_energy_scanner.rb
@@ -7,8 +7,14 @@ class BluetoothLowEnergyScanner
   def self.detect(address)
     Open3.popen3(HCITOOL_LESCAN_CMD) do |stdin, stdout, stderr, wait_thr|
       sleep 4
-      `#{HCITOOL_KILL_CMD}`
+      kill_scan
       return stdout.read.include?(address)
     end
+  end
+
+  private
+
+  def self.kill_scan
+    `#{HCITOOL_KILL_CMD}`
   end
 end
