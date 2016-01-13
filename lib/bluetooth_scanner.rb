b91195a0884985111227d6f6bce9f6619f482541
Isaac Datlof <ike18t@gmail.com>
2016-01-13 14:17:37 -0500

    Adding a grep for device name on hcitool info command
    Initial bluetooth command would give false positives

--- lib/bluetooth_scanner.rb
+++ lib/bluetooth_scanner.rb
@@ -1,7 +1,7 @@
 require 'open3'
 
 class BluetoothScanner
-  HCITOOL_INFO_CMD = 'sudo hcitool info %s'
+  HCITOOL_INFO_CMD = 'sudo hcitool info %s | grep \'Device Name\''
 
   def self.detect(address)
     Open3.popen3(HCITOOL_INFO_CMD % address) do |_, _, _, wait_thr|
